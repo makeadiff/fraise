279fe0cd0767da35d1fd13d856d64b03a31713b2
rhit99900 <rohitcse081@gmail.com>
2018-08-18 14:55:58 +0530

    Additional Fields

--- pledge.php
+++ pledge.php
@@ -12,6 +12,9 @@
   $network_info = $fraise->get_network($network_id);
   // dump($network_info);
 
+  if($network_info['pledge_type']=='nach'){
+    $network_info['pledged_amount'] = $network_info['pledged_amount']/str_replace('+','',$network_info['nach_duration']);
+  }
   // $network_data = $fraise->get_network_info($user_id);
 
   render();
